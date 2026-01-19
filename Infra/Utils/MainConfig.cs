666ad8b1c787af77b01cd9ecf7cf29bd6ca435d0
Igor <igorfain@gmail.com>
2026-01-19 11:19:29 +0200

    null exception warning

--- Infra/Utils/MainConfig.cs
+++ Infra/Utils/MainConfig.cs
@@ -1,9 +1,9 @@
 ﻿
-namespace Infra.Utils
+namespace automationexerciseTests.Infra.Utils
 {
     public class MainConfig
     {
-        public string url { get; set; }
-        public List<string> chromeArguments { get; set; }
+        public string url { get; set; } = string.Empty;
+        public List<string> chromeArguments { get; set; } = new List<string>();
     }
 }
